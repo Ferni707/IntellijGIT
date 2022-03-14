a47ab0363bfc0560f8f1208991c434622301009c
Fernando <fertrillop@gmail.com>
2022-03-14 22:28:59 +0100

    Commit v2 Fernando

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	Persona o = new Persona();
+	Persona p = new Persona();
+    p.nombre = "Pepa";
+    p.edad = 18;
+    p.estatura = 1.89;
+    System.out.println(p.nombre);
     }
 }
